87f8edc5aa595bfaaa930d95f35fbb486db270ba
maclacerda <markos.lacerda@gmail.com>
2021-06-18 15:44:05 -0300

    Initial Commit

---
+++ MLNetworkLayer/MLNetworkLayer.h
@@ -0,0 +1,18 @@
+//
+//  MLNetworkLayer.h
+//  MLNetworkLayer
+//
+//  Created by Marcos Lacerda on 18/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MLNetworkLayer.
+FOUNDATION_EXPORT double MLNetworkLayerVersionNumber;
+
+//! Project version string for MLNetworkLayer.
+FOUNDATION_EXPORT const unsigned char MLNetworkLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLNetworkLayer/PublicHeader.h>
+
+
