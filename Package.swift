451504c6e36223221f65fbd226e213b27105748c
maclacerda <markos.lacerda@gmail.com>
2021-06-22 09:10:59 -0300

    Update SPM file

--- Package.swift
+++ Package.swift
@@ -10,12 +10,13 @@ let package = Package(
         .library(
             name: "MLNetwork",
             targets: ["MLNetworkLayer"]
-        ),
-        targets: [
-            .binaryTarget(
-                name: "MLNetwork",
-                path: "./binary-framework/MLNetwork.xcframework"
-            )
-        ]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MLNetwork",
+            url: "https://github.com/maclacerda/MLNetwork/raw/master/binary-framework/MLNetwork.xcframework.zip",
+            checksum: "7c02bcd0075bcc0ab1e5ebe0b9d41017bcfe45b4b34b3a6b4f4a3dcad7aef045"
+        )
     ]
 )
